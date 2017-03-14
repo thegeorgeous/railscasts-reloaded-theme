cf8823250338ec9ac18d87768c64daa9e336e8a6
George Thomas <iamgeorgethomas@gmail.com>
2017-03-14 14:08:26 +0530

    Add GNU GPLv3 license

--- railscasts-reloaded-theme.el
+++ railscasts-reloaded-theme.el
@@ -6,6 +6,19 @@
 ;; URL: https://github.com/thegeorgeous/railscasts-reloaded-theme
 ;; Version: 1.5.0
 ;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
 ;;
 ;; The awesome Railscasts theme as used by Ryan Bates
